c67d5e8c5d14e22779dc8db3e0e35c6c91fd544d
Birat <gotofreelancing1@gmail.com>
2024-12-02 21:12:15 +0545

    Scanner, Conditions

--- src/test/Calculator.java
+++ src/test/Calculator.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
@@ -7,10 +9,21 @@ public class Calculator {
 //		System.out.println("Hello World!");
 		
 //		1. Get the number of hours worked
-		int hours = 40;
+		int hours = 0;
+		System.out.println("How many hours did you work?");
+		
+		Scanner scanner = new Scanner(System.in);
+		hours = scanner.nextInt();
+		
+		System.out.println("You work" + " "  + hours + " " + "hours.");
 		
 //		2. Get the hourly pay rate
-		double payRate = 25.50;
+		double payRate = 0;
+		System.out.println("What's your payrate ?");
+
+		payRate = scanner.nextDouble();
+		
+		System.out.println("Your payrate is " + payRate);
 		
 //		3. Multiply the hours and pay rate
 		double grossPay = hours * payRate;
@@ -18,4 +31,5 @@ public class Calculator {
 //		4. Display Result 
 		System.out.println("Your GrossPay is : " + grossPay);	
 	}
+	
 }
